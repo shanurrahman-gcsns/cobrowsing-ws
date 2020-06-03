f6a0fa024f8c2f5bae024cd2ff931b1178e40994
shanur <shanur.rahman@gamechangesns.com>
2020-06-03 14:21:57 +0400

    feat: [BN-0000] changing to use https

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var app = require("express")();
+const express = require('express');
+var app = express();
 var http = require("http").Server(app);
+
 
 var io = require("socket.io")(http);
 
+
+app.use(express.static(__dirname+'/public'));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 io.on("connection", (socket) => {
@@ -47,6 +52,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+http.listen(8080, () => {
+  console.log("listening on *:8080");
 });
